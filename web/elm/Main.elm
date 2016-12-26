7932047ed1cc4a45c1d736cedf12a8a0873abf71
kyrimsteele <kyrimcsteele@gmail.com>
2016-12-27 01:22:13 +1100

    added colouring to nutrient progress

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -2,18 +2,32 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Color exposing (..)
 
 
 -- Model
 
 
 type alias Model =
-    List ( Float, String )
+    List ( String, Int )
 
 
 initialModel : Model
 initialModel =
-    []
+    [ ( "Biotin", 5 )
+    , ( "Folate", 10 )
+    , ( "Vitamin A", 15 )
+    , ( "Vitamin B1", 16 )
+    , ( "Vitamin B2", 12 )
+    , ( "Vitamin B3", 13 )
+    , ( "Vitamin B5", 17 )
+    , ( "Vitamin B6", 35 )
+    , ( "Vitamin B12", 32 )
+    , ( "Vitamin C", 60 )
+    , ( "Vitamin D", 100 )
+    , ( "Vitamin E", 20 )
+    , ( "Vitamin K", 50 )
+    ]
 
 
 init : ( Model, Cmd Msg )
@@ -55,28 +69,57 @@ topSection =
 
 nutrientProgress : String -> Int -> Html Msg
 nutrientProgress label percentage =
-    div [ class "o-grid__cell" ]
-        [ div [ class "progress-label" ]
-            [ text label ]
-        , div
-            [ class "c-progress u-medium progress" ]
-            [ div
-                [ class "c-progress__bar"
-                , style
-                    [ ( "width", toString (percentage) ++ "%" )
+    let
+        colour =
+            if percentage <= 20 then
+                "#FF3D7F"
+            else if percentage <= 50 then
+                "#FFAB2E"
+            else if percentage <= 80 then
+                "#7FC7AF"
+            else
+                "#6ABE6E"
+    in
+        div [ class "o-grid__cell" ]
+            [ div [ class "progress-label" ]
+                [ span [] [ text label ]
+                , span
+                    [ class "progress-percentage"
+                    , style [ ( "color", colour ) ]
                     ]
+                    [ text ((toString percentage) ++ "%") ]
                 ]
-                [ text (toString (percentage) ++ "%") ]
+            , div
+                [ class "c-progress u-medium progress" ]
+                [ div
+                    [ class "c-progress__bar"
+                    , style
+                        [ ( "width", toString (percentage) ++ "%" )
+                        , ( "background-color", colour )
+                        ]
+                    ]
+                    []
+                ]
             ]
-        ]
+
+
+createNutrientProgress : ( String, Int ) -> Html Msg
+createNutrientProgress ( label, percentage ) =
+    nutrientProgress label percentage
 
 
 progressSection : Model -> Html Msg
 progressSection model =
     div [ class "o-grid--large-fit" ]
-        [ nutrientProgress "Vitamin A" 40
-        , nutrientProgress "Vitamin B" 10
-        ]
+        ([ div
+            [ class "o-grid__cell" ]
+            [ h2 [ class "c-heading" ] [ text "Vitamins" ] ]
+         ]
+            ++ (List.map
+                    createNutrientProgress
+                    model
+               )
+        )
 
 
 view : Model -> Html Msg
@@ -86,6 +129,7 @@ view model =
         , div
             [ class "o-grid" ]
             [ div [ class "o-grid__cell" ] [ progressSection model ]
+            , div [ class "o-grid__cell" ] [ text "blah" ]
             , div [ class "o-grid__cell" ] [ text "blah" ]
             ]
         ]
