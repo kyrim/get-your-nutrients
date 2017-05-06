b62a4abb475e4dd693b801654b1536922cb30cbc
kyrimsteele <kyrimcsteele@gmail.com>
2017-05-07 02:19:16 +1000

    Add more nutrient descriptions

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ var foodById = _.keyBy(foods, food => food.id);
 app.use(compression())
 app.use(express.static('dist'));
 
-  app.get('*', function(req, res) {
-      res.sendfile('./dist/index.html'); // load our public/index.html file
-  });
-
 app.get('/api/food/:foodId', function (req, res) {
   res.send(foodById[req.params.foodId]);
 });
+
+  app.get('*', function(req, res) {
+      res.sendFile('/dist/index.html');
+  });
 
 app.listen(3000, function () {
     console.log("Listening on port 3000");
